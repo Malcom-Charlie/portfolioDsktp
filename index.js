196879a7d6a65c6a7698e334cd1276ff7b6a3568
Malcom Charles <61559987+Malcom-Charlie@users.noreply.github.com>
2023-02-01 02:24:56 +0300

    Html structure Modal

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable prefer-destructuring */
+import data from './portfoliodata.js';
+
 const actvtMdl = document.getElementById('hamburger-menu');
 const dctvtMenu = document.querySelector('.hamburger');
 const body = document.querySelector('body');
@@ -7,7 +11,7 @@ const mdlLks = document.querySelectorAll('.modal__navigation__li');
 
 actvtMdl.addEventListener('click', (event) => {
   event.stopPropagation();
-  body.classList.add('stop-scrolling');
+  // body.classList.add('stop-scrolling');
   modal.classList.remove('display-none');
   dctvtMenu.classList.add('display-none');
 });
@@ -29,12 +33,68 @@ for (let i = 0; i < mdlLks.length; i += 1) {
 }
 
 // Portfolio Section
-import data from './portfoliodata.js';
 const workSectn = document.getElementById('portfolio');
+
+// function creates project details
+function projectDetails(obj) {
+  const container = document.getElementById('project-details');
+  body.classList.add('stop-scrolling');
+  container.classList.remove('display-none');
+  // Create Modal elements
+  const h2 = document.createElement('h2');
+  const tagsDiv = document.createElement('div');
+  tagsDiv.classList.add('tags');
+  const tag1 = document.createElement('p');
+  const bullet1 = document.createElement('img');
+  bullet1.classList.add('bullet');
+  const tag2 = document.createElement('span');
+  const bullet2 = document.createElement('img');
+  bullet2.classList.add('bullet');
+  const tag3 = document.createElement('span');
+  const mainImg = document.createElement('img');
+  const description = document.createElement('p');
+  description.classList.add('description');
+  const techDiv = document.createElement('ul');
+  techDiv.classList.add('technologies');
+  for (let i = 0; i < obj.technologies.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = obj.technologies[i];
+    li.classList.add('technologies__li');
+    techDiv.appendChild(li);
+  }
+  const buttonsDiv = document.createElement('div');
+  const button1 = document.createElement('button');
+  button1.classList.add('primary-button');
+  const btnImg1 = document.createElement('img');
+  const btnImg2 = document.createElement('img');
+  const button2 = document.createElement('button');
+  // Add content
+  h2.innerText = obj.title;
+  tag1.innerText = obj.tags[0];
+  tag2.innerText = obj.tags[1];
+  tag3.innerText = obj.tags[2];
+  bullet1.src = './images/circleicn.png';
+  bullet2.src = './images/circleicn.png';
+  mainImg.src = obj.image;
+  description.innerText = obj.description;
+  btnImg1.src = './img/see-live.svg';
+  btnImg2.src = './img/github-in-button.svg';
+  button1.classList.add('projectDetails__button');
+  button2.classList.add('projectDetails__button');
+  // Append
+  container.append(h2, tagsDiv, mainImg, description, techDiv, buttonsDiv);
+  tagsDiv.append(tag1, bullet1, tag2, bullet2, tag3);
+  buttonsDiv.append(button1, button2);
+  buttonsDiv.classList.add('projectDetails__buttons');
+  button1.innerText = 'See live';
+  button1.appendChild(btnImg1);
+  button2.innerText = 'See source';
+  button2.appendChild(btnImg2);
+}
 
 // To iterate elements in portfolio data
 for (let i = 0; i < data.length; i++) {
-  //Creating elements
+  // Creating elements
   const portDiv = document.createElement('div');
   portDiv.classList.add('port1');
   const portImg = document.createElement('img');
@@ -53,8 +113,8 @@ for (let i = 0; i < data.length; i++) {
   const span3 = document.createElement('span');
   const description = document.createElement('p');
   const tchnlgs = document.createElement('ul');
-  tchnlgs.classList.add('tags')
-  // Technologies for loop 
+  tchnlgs.classList.add('tags');
+  // Technologies for loop
   for (let j = 0; j < data[i].technologies[j]; j++) {
     const li = document.createElement('li');
     li.classList.add('tgs');
@@ -68,7 +128,7 @@ for (let i = 0; i < data.length; i++) {
   btnTxt.classList.add('btntxt');
   // Adding content
   portImg.src = data[i].image;
-  portImg.alt = data[i].title + ' img';
+  portImg.alt = `${data[i].title} img`;
   portTtl1.innerText = data[i].title;
   span1.innerText = data[i].tags[0];
   span2.innerText = data[i].tags[1];
@@ -76,7 +136,7 @@ for (let i = 0; i < data.length; i++) {
   bullet.src = 'images/circleicn.png';
   bullet2.src = 'images/circleicn.png';
   description.innerText = data[i].description;
-  // Append 
+  // Append
   workSectn.appendChild(portDiv);
   portDiv.appendChild(portImg);
   portDiv.appendChild(rghtDiv);
@@ -91,7 +151,9 @@ for (let i = 0; i < data.length; i++) {
   rghtDiv.appendChild(tchnlgs);
   portDiv.appendChild(btnDiv);
   btnDiv.appendChild(btn);
-  btnTxt.innerText = 'See Project'
+  btnTxt.innerText = 'See Project';
   btn.appendChild(btnTxt);
+  btn.addEventListener('click', () => {
+    projectDetails(data[i]);
+  });
 }
-  
