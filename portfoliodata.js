0d8307dbf206539ddf8bd90f7d4ad69452f9c68f
Malcom Charles <61559987+Malcom-Charlie@users.noreply.github.com>
2023-01-31 20:38:02 +0300

    Create portfolio data

---
+++ portfoliodata.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    title: 'Movie App',
+    tags: ['Freelance', 'Front-end', '2022'],
+    description: 'A project that uses MovieDB API for getting information about the most viewed movies of the last seven days.',
+    technologies: ['React', 'API', 'JavaScript'],
+    link: 'https://davidespinog.github.io/platzi_movies_react/',
+    image: './img/portfolio1.jpg',
+  },
+  {
+    title: 'Data example 1',
+    tags: ['CANOPY', 'Back-End', '2015'],
+    description: 'A daily selection of privately personalized reads. No accounts or sign-ups required.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://davidespinog.github.io/platzi_movies_react/',
+    image: './img/portfolio2.jpg',
+  },
+  {
+    title: 'Data example 2',
+    tags: ['CANOPY', 'Back-End', '2015'],
+    description: 'A daily selection of privately personalized reads. No accounts or sign-ups required.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://davidespinog.github.io/platzi_movies_react/',
+    image: './img/portfolio3.jpg',
+  },
+  {
+    title: 'Data example 2',
+    tags: ['CANOPY', 'Back-End', '2015'],
+    description: 'A daily selection of privately personalized reads. No accounts or sign-ups required.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://davidespinog.github.io/platzi_movies_react/',
+    image: './img/portfolio3.jpg',
+  },
+];
+
+export default data;
